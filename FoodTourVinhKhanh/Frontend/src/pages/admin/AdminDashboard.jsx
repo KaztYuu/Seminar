ccb6127d18356155ea231ce56a649b86fe54924e
KaztYuu <92777635+KaztYuu@users.noreply.github.com>
2026-05-09 07:48:53 +0700

    Update AdminDashboard.jsx

--- FoodTourVinhKhanh/Frontend/src/pages/admin/AdminDashboard.jsx
+++ FoodTourVinhKhanh/Frontend/src/pages/admin/AdminDashboard.jsx
@@ -183,7 +183,7 @@ export default function AdminDashboard() {
                   type="number"
                   value={maxLimit}
                   onChange={(e) => setMaxLimit(parseInt(e.target.value))}
-                  className="w-full px-4 py-2.5 bg-gray-50 border border-gray-200 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-blue-500 font-bold text-center transition-all"
+                  className="w-full px-4 py-2.5 bg-gray-50 border border-gray-200 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-blue-500 font-bold text-center text-black transition-all"
                 />
               </div>
               <button
@@ -192,7 +192,7 @@ export default function AdminDashboard() {
                 className="flex items-center gap-2 bg-slate-900 hover:bg-slate-800 text-white px-6 py-2.5 rounded-xl font-bold transition-all disabled:opacity-50 active:scale-95 shadow-lg shadow-slate-200"
               >
                 {isUpdating ? "..." : <Save size={18} />}
-                Lưu cấu hình
+                Lưu
               </button>
             </div>
           </div>
