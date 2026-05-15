348e9d93d3a96dad64e8c2d1136dd3418f94441e
KaztYuu <92777635+KaztYuu@users.noreply.github.com>
2026-05-15 12:25:36 +0700

    Update api.jsx

--- FoodTourVinhKhanh/Frontend/src/utils/api.jsx
+++ FoodTourVinhKhanh/Frontend/src/utils/api.jsx
@@ -26,7 +26,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     const message = error.response?.data?.detail || "Có lỗi xảy ra";
-    const isAuthPage = ["/login", "/", "/signup", "/server-overload"].includes(
+    const isAuthPage = ["/login", "/", "/signup", "/server-overload", "/tourist/explore", "/tourist/tours"].includes(
       window.location.pathname,
     );
 
